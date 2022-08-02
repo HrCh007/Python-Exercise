253914a6633599b0cb9ed114b98f1ebf24739aab
HrCh007 <chowdhryhrithik@gmail.com>
2022-08-02 11:45:12 +0530

    second commit

--- IO_exercise.py
+++ IO_exercise.py
@@ -3,3 +3,10 @@
 num = 8
 print('%o' % num)
 
+# Exercise 7: Accept any three string from one input() call
+
+str1, str2, str3 = input("Enter three string").split()
+print('Name1:', str1)
+print('Name2:', str2)
+print('Name3:', str3)
+
