7516952dd1ca73a9cce16e00a6e900061053a34b
HrCh007 <chowdhryhrithik@gmail.com>
2022-08-06 13:26:26 +0530

    tenth commit

--- Set_exercise.py
+++ Set_exercise.py
@@ -22,7 +22,34 @@ print(set1.union(set2))
 
 # Exercise 4: Update the first set with items that don’t exist in the second set
 
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+
+set1.difference_update(set2)
+print(set1)
+
+# Exercise 6: Return a set of elements present in Set A or B, but not both
+
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 
-print(set1.difference_update(set2))
+print(set1.symmetric_difference(set2))
+
+# Exercise 7: Check if two sets have any elements in common. If yes, display the common elements
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {60, 70, 80, 90, 10}
+
+if set1.isdisjoint(set2):
+  print("Two sets have no items in common")
+else:
+  print("Two sets have items in common")
+  print(set1.intersection(set2))
+
+# Exercise 9: Remove items from set1 that are not common to both set1 and set2
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+set1.intersection_update(set2)
+print(set1)
---
+++ Tuple_exercise.py
@@ -0,0 +1,40 @@
+# Exercise 1: Reverse the tuple
+
+tuple1 = (10, 20, 30, 40, 50)
+tuple1 = tuple1[::-1]
+print(tuple1)
+
+# Exercise 4: Unpack the tuple into 4 variables
+
+tuple1 = (10, 20, 30, 40)
+
+a, b, c, d = tuple1
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Exercise 5: Swap two tuples in Python
+
+tuple1 = (11, 22)
+tuple2 = (99, 88)
+tuple1, tuple2 = tuple2, tuple1
+print(tuple2)
+print(tuple1)
+
+# Exercise 7: Modify the tuple
+
+tuple1 = (11, [22, 33], 44, 55)
+tuple1[1][0] = 222
+print(tuple1)
+
+# Exercise 8: Sort a tuple of tuples by 2nd item
+
+tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29))
+tuple1 = tuple(sorted(list(tuple1), key=lambda x: x[1]))
+print(tuple1)
+
+# Exercise 9: Counts the number of occurrences of item 50 from a tuple
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple1.count(50))
