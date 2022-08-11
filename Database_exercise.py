79bccc514d922e280f5b6642cc23104915f7759f
HrCh007 <chowdhryhrithik@gmail.com>
2022-08-11 19:38:12 +0530

    seventeenth commit

--- Database_exercise.py
+++ Database_exercise.py
@@ -70,4 +70,103 @@ print("\n")
 get_doctor_detail(105)
 print('\n')
 
-# 
+# Exercise 3: Get the list Of doctors as per the given specialty and salary
+
+def get_specialist_doctors_list(speciality, salary):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        select_query = """select * from Doctor where speciality = ? AND salary > ?"""
+        cursor.execute(select_query, (speciality,salary,))
+        records = cursor.fetchall()
+        print("Printing doctors whose specialty is", speciality, "and salary greater than", salary, "\n")
+        for row in records:
+            print("Doctor Id: ", row[0])
+            print("Doctor Name:", row[1])
+            print("Hospital Id:", row[2])
+            print("Joining Date:", row[3])
+            print("Specialty:", row[4])
+            print("Salary:", row[5])
+            print("Experience:", row[6], "\n")
+        close_connection(connection)
+    except (Exception, sqlite3.Error) as error:
+        print("Error while getting data", error)
+
+print("Question 3: Get Doctors as per given Speciality\n")
+get_specialist_doctors_list("Garnacologist", 30000)
+
+# Exercise 4: Get a list of doctors from a given hospital
+
+def get_hospital_name(hospital_id):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        select_query = """select * from Hospital where Hospital_Id = ?"""
+        cursor.execute(select_query, (hospital_id,))
+        record = cursor.fetchone()
+        close_connection(connection)
+        return record[1]
+    except (Exception, sqlite3.Error) as error:
+        print("Error while getting data", error)
+
+def get_doctors(hospital_id):
+    try:
+        hospital_name = get_hospital_name(hospital_id)
+        connection = get_connection()
+        cursor = connection.cursor()
+        sql_select_query = """select * from Doctor where Hospital_Id = ?"""
+        cursor.execute(sql_select_query, (hospital_id,))
+        records = cursor.fetchall()
+
+        print("Printing Doctors of ", hospital_name, "Hospital")
+        for row in records:
+            print("Doctor Id:", row[0])
+            print("Doctor Name:", row[1])
+            print("Hospital Id:", row[2])
+            print("Hospital Name:", hospital_name)
+            print("Joining Date:", row[3])
+            print("Specialty:", row[4])
+            print("Salary:", row[5])
+            print("Experience:", row[6], "\n")
+        close_connection(connection)
+    except (Exception, sqlite3.Error) as error:
+        print("Error while getting doctor's data", error)
+
+print("Question 4:  Get List of doctors of a given Hospital Id\n")
+get_doctors(2)
+
+# Operation 5: Update doctor experience in years
+
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+def update_doctor_experience(doctor_id):
+    # Update Doctor Experience in Years
+    try:
+        # Get joining date
+        connection = get_connection()
+        cursor = connection.cursor()
+        select_query = """select Joining_Date from Doctor where Doctor_Id = ?"""
+        cursor.execute(select_query, (doctor_id,))
+        joining_date = cursor.fetchone()
+
+        # calculate Experience in years
+        joining_date_1 = datetime.datetime.strptime(''.join(map(str, joining_date)), '%Y-%m-%d')
+        today_date = datetime.datetime.now()
+        experience = relativedelta(today_date, joining_date_1).years
+
+        # Update doctor's Experience now
+        connection = get_connection()
+        cursor = connection.cursor()
+        sql_select_query = """update Doctor set Experience = ? where Doctor_Id = ?"""
+        cursor.execute(sql_select_query, (experience, doctor_id))
+        connection.commit()
+        print("Doctor Id:", doctor_id, " Experience updated to ", experience, " years")
+        close_connection(connection)
+
+    except (Exception, sqlite3.Error) as error:
+        print("Error while getting doctor's data", error)
+
+print("Question 5: Calculate and Update experience of all doctors  \n")
+update_doctor_experience(101)
