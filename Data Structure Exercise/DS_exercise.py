a5725a11d8f099427ae3715a2105cc597105c41e
HrCh007 <chowdhryhrithik@gmail.com>
2022-08-20 23:56:58 +0530

    twenty first commit

--- Data Structure Exercise/DS_exercise.py
+++ Data Structure Exercise/DS_exercise.py
@@ -9,6 +9,8 @@ res = l1[1::2] + l2[0::2]
 
 print(res)
 
+print('\n')
+
 # Exercise 4: Count the occurrence of each element from a list
 
 sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
@@ -20,9 +22,11 @@ for item in sample_list:
     else:
         count_dict[item] = 1
 
-# Exercise 5: Create a Python set such that it shows the element from both lists in a pair
-
 print("Printing count of each item  ", count_dict)
+
+print('\n')
+
+# Exercise 5: Create a Python set such that it shows the element from both lists in a pair
 
 first_list = [2, 3, 4, 5, 6, 7, 8]
 
@@ -31,6 +35,8 @@ second_list = [4, 9, 16, 25, 36, 49, 64]
 result = zip(first_list, second_list)
 result_set = set(result)
 print(result_set)
+
+print('\n')
 
 # Exercise 6: Find the intersection (common) of two sets and remove those elements from the first set
 
@@ -44,6 +50,8 @@ for item in intersection:
 
 print("First Set after removing common element ", first_set)
 
+print('\n')
+
 # Exercise 8: Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list
 
 roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
@@ -51,6 +59,8 @@ sample_dict = {'Jhon': 47, 'Emma': 69, 'Kelly': 76, 'Jason': 97}
 
 roll_number[:] = [item for item in roll_number if item in sample_dict.values()]
 print("after removing unwanted elements from list:", roll_number)
+
+print('\n')
 
 # Exercise 9: Get all values from the dictionary and add them to a list but don’t add duplicates
 
@@ -63,3 +73,5 @@ for item in speed.values():
         speed_list.append(item)
 
 print(speed_list)
+
+print('\n')
