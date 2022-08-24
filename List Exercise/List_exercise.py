b4c725a8a5c1ad62b9fb0aefb5036f1b9c359e5a
HrCh007 <chowdhryhrithik@gmail.com>
2022-08-24 23:16:55 +0530

    twenty second commit

--- List Exercise/List_exercise.py
+++ List Exercise/List_exercise.py
@@ -5,12 +5,16 @@ list2 = ["y", "me", "s", "lly"]
 list3 = [i + j for i, j in zip(list1, list2)]
 print(list3)
 
+print('\n')
+
 # Exercise 3: Turn every item of a list into its square
 
 numbers = [1, 2, 3, 4, 5, 6, 7]
 
 squares = [ i * i for i in numbers]
 print(squares)
+
+print('\n')
 
 # Exercise 4: Concatenate two lists in the following order
 
@@ -23,6 +27,8 @@ for i in list1:
 
 print(list3)
 
+print('\n')
+
 # Exercise 5: Iterate both lists simultaneously
 
 list1 = [10, 20, 30, 40]
@@ -31,6 +37,8 @@ list2 = [100, 200, 300, 400]
 for x, y in zip(list1, list2[::-1]):
     print(x, y)
 
+print('\n')
+
 # Exercise 6: Remove empty strings from the list of strings
 
 list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
@@ -38,12 +46,16 @@ list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
 res = list(filter(None, list1))
 print(res)
 
+print('\n')
+
 # Exercise 7: Add new item to list after a specified item
 
 list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
 
 list1[2][2].append(7000)
 print(list1)
+
+print('\n')
 
 # Exercise 10: Remove all occurrences of a specific item from a list.
 
@@ -53,4 +65,5 @@ list1 = [5, 20, 15, 20, 25, 50, 20]
 res = [ i for i in list1 if i != val]
 print(res)
 
+print('\n')
 
